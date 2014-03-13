346e0a6df898cd18c21be4fcd84a752f1dc68d5d
dead_horse <dead_horse@qq.com>
2014-03-14 01:11:21 +0800

    add test for defer

---
+++ test/defer.test.js
@@ -0,0 +1,126 @@
+/**!
+ * koa-session - test/koa-session.test.js
+ * Copyright(c) 2013
+ * MIT Licensed
+ *
+ * Authors:
+ *   dead_horse <dead_horse@qq.com> (http://deadhorse.me)
+ */
+
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+
+var Session = require('..');
+var koa = require('koa');
+var app = require('./support/defer');
+var request = require('supertest');
+var mm = require('mm');
+var should = require('should');
+var EventEmitter = require('events').EventEmitter;
+
+describe('test/defer.test.js', function () {
+
+  describe('use', function () {
+    var cookie;
+    var mockCookie = 'koa.sid=s:dsfdss.PjOnUyhFG5bkeHsZ1UbEY7bDerxBINnZsD5MUguEph8; path=/; httponly';
+    it('should GET /session/get ok', function (done) {
+      request(app)
+      .get('/session/get')
+      .expect(/1/)
+      .end(function (err, res) {
+        cookie = res.headers['set-cookie'].join(';');
+        done();
+      });
+    });
+
+    it('should GET /session/get second ok', function (done) {
+      request(app)
+      .get('/session/get')
+      .set('cookie', cookie)
+      .expect(/2/, done);
+    });
+
+    it('should GET /session/httponly ok', function (done) {
+      request(app)
+      .get('/session/httponly')
+      .set('cookie', cookie)
+      .expect(/httpOnly: false/, function (err, res) {
+        should.not.exist(err);
+        cookie = res.headers['set-cookie'].join(';');
+        cookie.indexOf('httponly').should.equal(-1);
+        cookie.indexOf('expires=').should.above(0);
+        request(app)
+        .get('/session/get')
+        .set('cookie', cookie)
+        .expect(/3/, done);
+      });
+    });
+
+    it('should GET /session/httponly twice ok', function (done) {
+      request(app)
+      .get('/session/httponly')
+      .set('cookie', cookie)
+      .expect(/httpOnly: true/, function (err, res) {
+        should.not.exist(err);
+        cookie = res.headers['set-cookie'].join(';');
+        cookie.indexOf('httponly').should.above(0);
+        cookie.indexOf('expires=').should.above(0);
+        done();
+      });
+    });
+
+    it('should another user GET /session/get ok', function (done) {
+      request(app)
+      .get('/session/get')
+      .expect(/1/, done);
+    });
+
+    it('should GET /session/nothing ok', function (done) {
+      request(app)
+        .get('/session/nothing')
+        .set('cookie', cookie)
+        .expect(/3/, done);
+    });
+
+    it('should wrong cookie GET /session/get ok', function (done) {
+      request(app)
+      .get('/session/get')
+      .set('cookie', mockCookie)
+      .expect(/1/, done);
+    });
+
+    it('should wrong cookie GET /session/get twice ok', function (done) {
+      request(app)
+      .get('/session/get')
+      .set('cookie', mockCookie)
+      .expect(/1/, done);
+    });
+
+    it('should GET /session/remove ok', function (done) {
+      request(app)
+      .get('/session/remove')
+      .set('cookie', cookie)
+      .expect(/0/, function () {
+        request(app)
+        .get('/session/get')
+        .set('cookie', cookie)
+        .expect(/1/, done);
+      });
+    });
+
+    it('should GET / error by session ok', function (done) {
+      request(app)
+      .get('/')
+      .expect(/no session/, done);
+    });
+
+    it('should GET /session ok', function (done) {
+      request(app)
+      .get('/session')
+      .expect(/has session/, done);
+    });
+  });
+});
---
+++ test/support/defer.js
@@ -0,0 +1,105 @@
+/**!
+ * koa-session - test/support/defer.js
+ * Copyright(c) 2013
+ * MIT Licensed
+ *
+ * Authors:
+ *   dead_horse <dead_horse@qq.com> (http://deadhorse.me)
+ */
+
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var koa = require('koa');
+var http = require('http');
+var session = require('../../');
+
+var app = koa();
+
+app.name = 'koa-session-test';
+app.outputErrors = true;
+app.keys = ['keys', 'keykeys'];
+app.proxy = true; // to support `X-Forwarded-*` header
+
+app.use(session({
+  key: 'koss:test_sid',
+  cookie: {
+    maxAge: 86400,
+    path: '/session'
+  },
+  defer: true
+}));
+
+// will ignore repeat session
+app.use(session({
+  key: 'koss:test_sid',
+  cookie: {
+    maxAge: 86400,
+    path: '/session'
+  },
+  defer: true
+}));
+
+app.use(function *controllers() {
+  switch (this.request.url) {
+  case '/favicon.ico':
+  this.staus = 404;
+  break;
+  case '/nosession':
+    nosession(this);
+    break;
+  case '/session/get':
+    yield get(this);
+    break;
+  case '/session/nothing':
+    yield nothing(this);
+    break;
+  case '/session/remove':
+    yield remove(this);
+    break;
+  case '/session/httponly':
+    yield switchHttpOnly(this);
+    break;
+  default:
+    yield other(this);
+  }
+});
+
+function nosession(ctx) {
+  ctx.body = ctx._session ? 'has session' : 'no session';
+}
+
+function *nothing(ctx) {
+  ctx.body = (yield ctx.session).count;
+}
+
+function *get(ctx) {
+  var session = yield ctx.session;
+  session.count = session.count || 0;
+  session.count++;
+  ctx.body = session.count;
+}
+
+function *remove(ctx) {
+  ctx.session = null;
+  ctx.body = 0;
+}
+
+function *switchHttpOnly(ctx) {
+  var session = yield ctx.session;
+  var httpOnly = session.cookie.httpOnly;
+  session.cookie.httpOnly = !httpOnly;
+  ctx.body = 'httpOnly: ' + !httpOnly;
+}
+
+function *other(ctx) {
+  ctx.body = (yield ctx.session) ? 'has session' : 'no session';
+}
+
+app.on('error', function (err) {
+  console.error(err.stack);
+});
+// app.listen(7001)
+var app = module.exports = http.createServer(app.callback());
